96394d66a62039f35f2958fe08c9def8dc12582e
Abhilash S <abhilash.b@sysfore.com>
2024-04-29 21:09:29 +0530

    Updated Controller , implemented "GetBookByTitle" method

--- EBook Management Application/Controllers/BookAuthorController.cs
+++ EBook Management Application/Controllers/BookAuthorController.cs
@@ -29,5 +29,29 @@ namespace EBook_Management_Application.Controllers
             return Ok(books);
         }
 
+        [HttpDelete]
+        [Route("/DeleteBook/{id}")]
+        public ActionResult DeleteBook(Guid id)
+        {
+            bool result = _databasemanager.DeleteBook(id);
+            if (result)
+            {
+                return Accepted("Book deleted successfully");
+
+            }
+            return NotFound("Book not found");
+        }
+
+        [HttpGet]
+        [Route("/GetBookByTitle")]
+        public ActionResult GetBookByTitle(string title)
+        {
+            var books = _databasemanager.GetBooksByTitle(title);
+            if (books.Count == 0)
+            {
+                return NotFound("No book with given title");
+            }
+            return Ok(books);
+        }
     }
 }
