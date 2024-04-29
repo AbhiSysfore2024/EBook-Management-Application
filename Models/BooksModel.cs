d93622511c2ab85f33b97b69c1fbf21944b2da3d
abhilash.b <abhilash.b@BLRSFLT274>
2024-04-29 12:33:58 +0530

    Add project files.

---
+++ Models/BooksModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+
+namespace Models
+{
+    public class BooksModel
+    {
+        public Guid BookID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Guid ISBN { get; set; }
+        public DateTime Publication_Date {  get; set; }
+        public float Price {  get; set; }
+        public string Language {  get; set; }
+        public string Publisher { get; set; }
+        public int PageCount { get; set;}
+        public float AvgRating {  get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set;}
+        public Genre BookGenre { get; set; }
+
+        public BooksModel()
+        {
+
+        }
+
+        public BooksModel( DTOBooks books )
+        {
+            BookID = Guid.NewGuid();
+            CreatedAt = DateTime.Now;
+            this.Title = books.Title;
+            this.Description = books.Description;
+            this.ISBN = books.ISBN;
+            this.Publication_Date = books.Publication_Date;
+            this.Price = books.Price;
+            this.Language = books.Language;
+            this.Publisher = books.Publisher;
+            this.PageCount = books.PageCount;
+            this.AvgRating = books.AvgRating;
+            this.BookGenre = (Genre)books.BookGenre;
+        }
+    }
+}
---
+++ Models/DTOBooks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class DTOBooks
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Guid ISBN { get; set; }
+        public DateTime Publication_Date { get; set; }
+        public float Price { get; set; }
+        public string Language { get; set; }
+        public string Publisher { get; set; }
+        public int PageCount { get; set; }
+        public float AvgRating { get; set; }
+        public Genre BookGenre { get; set; }
+    }
+}
