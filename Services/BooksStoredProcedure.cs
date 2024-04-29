2d44739587b87a8837df486544191f87008e9d82
Abhilash S <abhilash.b@sysfore.com>
2024-04-29 19:28:01 +0530

    Implemented Delete operation in services

--- Services/BooksStoredProcedure.cs
+++ Services/BooksStoredProcedure.cs
@@ -12,25 +12,34 @@ namespace Services
 {
     public class BooksStoredProcedure : IDatabaseManager
     {
-        private readonly string _connectionString = "Data Source=192.168.10.28\\SQLEX2017;Initial Catalog=Abhilash;User Id=sysfore.ea;Password=Sys@2024#;Encrypt=false";
+      //  private readonly string _connectionString = "Data Source=192.168.10.28\\SQLEX2017;Initial Catalog=Abhilash;User Id=sysfore.ea;Password=Sys@2024#;Encrypt=false";
+        private readonly string _connectionString = "Data Source=BLRSFLT274\\SQLEXPRESS;Initial Catalog=ADOBookManagement;Integrated Security=True;Trusted_Connection=true;encrypt=false;";
 
         public List<BooksModel> GetAllBooks()
         {
             List<BooksModel> _bookDatabase = new List<BooksModel>();
 
             using SqlConnection connection = new SqlConnection(_connectionString);
-            connection.Open();
-            string selectQuery = "GetBookDetails";
-            SqlCommand command = new SqlCommand(selectQuery, connection);
-            command.CommandType = System.Data.CommandType.StoredProcedure;
-            SqlDataReader reader = command.ExecuteReader();
-
-            while (reader.Read())
+            try
             {
-                BooksModel emp = MapReaderBook(reader);
-                _bookDatabase.Add(emp);
+                connection.Open();
+                string selectQuery = "GetBookDetails";
+                SqlCommand command = new SqlCommand(selectQuery, connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    BooksModel emp = MapReaderBook(reader);
+                    _bookDatabase.Add(emp);
+                }
+                return _bookDatabase;
             }
-            return _bookDatabase;
+            catch (Exception e)
+            {
+                return [];
+            }
+
         }
 
         public BooksModel MapReaderBook(SqlDataReader reader)
@@ -41,11 +50,11 @@ namespace Services
             book.Description = reader.GetString(2);
             book.ISBN = reader.GetGuid(3);
             book.Publication_Date = reader.GetDateTime(4);
-            book.Price = reader.GetFloat(5);
+            book.Price = (float)reader.GetDouble(5);
             book.Language = reader.GetString(6);
             book.Publisher = reader.GetString(7);
             book.PageCount = reader.GetInt32(8);
-            book.AvgRating  =reader.GetFloat(9);
+            book.AvgRating = (float)reader.GetDouble(9);
             book.BookGenre = (Genre)reader.GetInt32(10);
             book.IsAvailable = reader.GetBoolean(11);
 
@@ -66,13 +75,13 @@ namespace Services
                 command.CommandType = System.Data.CommandType.StoredProcedure;
                 command.Parameters.Add("@BookID", SqlDbType.UniqueIdentifier).Value = book.BookID;
                 command.Parameters.Add("@Title", SqlDbType.VarChar).Value = book.Title;
-                command.Parameters.Add("@Description", SqlDbType.VarChar).Value = book.Description;
+                command.Parameters.Add("@Descriptions", SqlDbType.VarChar).Value = book.Description;
                 command.Parameters.Add("@ISBN", SqlDbType.UniqueIdentifier).Value = book.ISBN;
-                command.Parameters.Add("@Publication_Date", SqlDbType.DateTime).Value = book.Publication_Date;
+                command.Parameters.Add("@PublicationDate", SqlDbType.DateTime).Value = book.Publication_Date;
                 command.Parameters.Add("@Price", SqlDbType.Float).Value = book.Price;
-                command.Parameters.Add("@Language", SqlDbType.VarChar).Value = book.Language;
+                command.Parameters.Add("@BookLanguage", SqlDbType.VarChar).Value = book.Language;
                 command.Parameters.Add("@Publisher", SqlDbType.VarChar).Value = book.Publisher;
-                command.Parameters.Add("@PageCount", SqlDbType.Int).Value = book.PageCount;
+                command.Parameters.Add("@PagesCount", SqlDbType.Int).Value = book.PageCount;
                 command.Parameters.Add("@AvgRating", SqlDbType.Float).Value = book.AvgRating;
                 command.Parameters.Add("@BookGenre", SqlDbType.Int).Value = (int)book.BookGenre;
                 command.Parameters.Add("@IsAvailable", SqlDbType.Bit).Value = book.IsAvailable;
@@ -85,6 +94,26 @@ namespace Services
             catch (Exception e)
             {
                 return "Cannot add employee";
+            }
+        }
+
+        public bool DeleteBook(Guid id)
+        {
+            using SqlConnection connection = new SqlConnection(_connectionString);
+            try
+            {
+                connection.Open();
+                BooksModel book = new BooksModel();
+                string deleteQuery = "DeleteBook";
+                SqlCommand command = new SqlCommand(deleteQuery, connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@BookID", id);
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
             }
         }
     }
