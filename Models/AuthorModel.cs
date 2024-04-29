eb4958a9708deb4a28ced64fa5e246a30444682c
Abhilash S <abhilash.b@sysfore.com>
2024-04-29 12:54:52 +0530

    Implemented normal & DTO class for Authors table

---
+++ Models/AuthorModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class AuthorModel
+    {
+        public Guid AuthorID { get; set; }
+        public Name Name { get; set; }
+        public string Biography { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Country { get; set; }
+        private DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public AuthorModel()
+        {
+
+        }
+
+        public AuthorModel ( DTOAuthor author )
+        {
+            AuthorID = Guid.NewGuid();
+            CreatedAt = DateTime.Now;
+            this.Name = (Name)author.Name;
+            this.Biography = author.Biography;
+            this.BirthDate = author.BirthDate;
+            this.Country = author.Country;
+
+
+        }
+
+    }
+
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
---
+++ Models/DTOAuthor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class DTOAuthor
+    {
+        public Name Name { get; set; }
+        public string Biography { get; set; }
+        public DateTime BirthDate {  get; set; }
+        public string Country { get; set; }
+    }
+}
