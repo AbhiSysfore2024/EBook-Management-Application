767bd944f304d455c2947a1be4aaf22895ea30e8
Abhilash S <abhilash.b@sysfore.com>
2024-05-06 00:34:51 +0530

    Added new "UpdateAuthorModel" class

--- Models/DTOAuthor.cs
+++ Models/DTOAuthor.cs
@@ -13,4 +13,14 @@ namespace Models
         public DateTime BirthDate { get; set; }
         public string Country { get; set; }
     }
+
+    public class UpdateAuthorModel
+    {
+        public Guid AuthorID { get; set; }
+        public Name Name { get; set; }
+        public string Biography { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Country { get; set; }
+    }
+
 }
