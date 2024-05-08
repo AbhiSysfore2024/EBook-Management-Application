5a962631ddd69d2236b6e52d43ca754dba7e1cd2
Abhilash S <abhilash.b@sysfore.com>
2024-05-08 23:34:34 +0530

    Implemented "RoleAssigned" method in "LoginRequestStoredProcedure" service

---
+++ Services/LoginRequestStoredProcedure.cs
@@ -0,0 +1,54 @@
+﻿using AppSettings;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using Models;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class LoginRequestStoredProcedure : ILoginRequest
+    {
+        private readonly ConnectionStrings _connection;
+
+        public LoginRequestStoredProcedure(IOptions<ConnectionStrings> connection)
+        {
+            _connection = connection.Value;
+        }
+
+        public string RoleAssigned(DTOLoginRequest loginRequest)
+        {
+            string role = "";
+            using (SqlConnection connection = new SqlConnection(_connection.SQLServerManagementStudio))
+            {
+                try
+                {
+                    connection.Open();
+                    string returnRoleQuery = "GetRole";
+                    SqlCommand command = new SqlCommand(returnRoleQuery, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.Add("@Username", SqlDbType.VarChar).Value = loginRequest.UserName;
+                    command.Parameters.Add("@Password", SqlDbType.VarChar).Value = loginRequest.PassWord;
+
+                    var result = command.ExecuteScalar();
+                    if (result != null)
+                    {
+                        role = Convert.ToString(result);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return role;
+        }
+
+    }
+}
